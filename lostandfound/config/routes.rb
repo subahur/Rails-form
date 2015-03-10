d65f34113f765e6957548429ac3027a687ce05b5
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-09 23:04:05 -0400

    added routes items/search

--- lostandfound/config/routes.rb
+++ lostandfound/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :items
 
+  resources :items do
+    collection do
+      get 'search'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

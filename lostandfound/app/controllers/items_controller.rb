d34d38287e43da3418434d223bbbdf86b6a26588
Subahu Rayamajhi <subahur@brandeis.edu>
2015-03-10 22:00:54 -0400

    Working SEARCH

--- lostandfound/app/controllers/items_controller.rb
+++ lostandfound/app/controllers/items_controller.rb
@@ -12,6 +12,10 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def search
+    @items = Item.search(params[:search])
+  end
+
   # GET /items/new
   def new
     @item = Item.new
@@ -72,7 +76,5 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:title, :description, :item_type)
     end
 
-    def search
-
-    end 
+  
 end
--- lostandfound/config/routes.rb
+++ lostandfound/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :items do
     collection do
-      get 'search'
+      post 'search'
     end
   end
   # The priority is based upon order of creation: first created -> highest priority.
